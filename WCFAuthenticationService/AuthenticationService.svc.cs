1e31994fb0f3dc7aa443b7de9fd69fef1c2b1c29
kedrianjames <keg1082@yahoo.com>
2012-12-07 20:25:16 +0200

    initial commit

---
+++ WCFAuthenticationService/AuthenticationService.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Security;
+
+namespace WCFAuthenticationService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    public class AuthenticationService : IAuthenticationService
+    {
+        //validates a user
+        public Boolean isValidCredentials(string Username, string Password)
+        {
+
+            if (Membership.ValidateUser(Username, Password) == true) return true;
+
+            return false;
+
+        }
+    }
+}
